6cf0ea35bc1baa035198d4a89e0fcf1b57f18532
nanadyaaa <nadyaalestari962@gmail.com>
2024-03-13 15:49:10 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HaiController;
- 
+
+
 Route::get('/postech/{sklh}/cek', [HaiController::class, 'index']);
 
-Route::get('/Nanad', function () {
-    return view('welcome');
-});
+Route::get('/dashboard',  [AuthController::class, 'dashboard'])->name('dasboard');
+Route::get('/', [AuthController::class, 'dashboard']); 
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+//Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+// Route::get('/users', [UserController::class, 'index'])->name('users');
+
